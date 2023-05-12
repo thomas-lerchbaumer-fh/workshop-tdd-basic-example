06d510b4ca3336dd81c5787aa1a86f7a916d46e5
thomas-lerchbaumer-fh <71784753+thomas-lerchbaumer-fh@users.noreply.github.com>
2023-05-12 17:37:14 +0200

    *merge

--- src/main/java/pt/ipp/isep/dei/examples/tdd/basic/domain/Calculator.java
+++ src/main/java/pt/ipp/isep/dei/examples/tdd/basic/domain/Calculator.java
@@ -1,5 +1,7 @@
 package pt.ipp.isep.dei.examples.tdd.basic.domain;
 
+import static java.lang.Integer.MAX_VALUE;
+
 /**
  * Calculator class.
  * This class is very simple in order to demonstrate how to build test cases for Unit Testing.
@@ -13,8 +15,12 @@ public class Calculator {
      * @param secondOperand Second Operand to sum.
      * @return The sum of firstOperand with secondOperand.
      */
-    public int sum(int firstOperand, int secondOperand) {
-        return firstOperand + secondOperand;
+    public int sum(int firstOperand, int secondOperand) throws ArithmeticException {
+            int res = firstOperand + secondOperand;
+            //if(res > MAX_VALUE || res < Integer.MIN_VALUE) throw new ArithmeticException("overflow");
+
+           return firstOperand + secondOperand;
+
     }
 
     public int subtract(int firstOperand, int secondOperand) {
@@ -22,7 +28,10 @@ public class Calculator {
     }
 
     public int divide(int dividend, int divisor) {
-        throw new UnsupportedOperationException();
+        if(divisor == 0)
+            throw new ArithmeticException("Division by zero");
+
+        return dividend / divisor;
     }
 
     public int multiply(int firstOperand, int secondOperand) {
--- src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/CalculatorTest.java
+++ src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/CalculatorTest.java
@@ -164,7 +164,156 @@ public class CalculatorTest {
         // Assert
         assertEquals(expectedResult, result);
     }
+
+    /**
+     * Test to ensure positive and negative numbers are summed correctly.<p>
+     * <p>
+     * For demonstration purposes the Arrange/Act/Assert syntax is used:<p>
+     * Arranje a positive number (three) and a negative number (minus two)<p>
+     * Act I sum three to minus two<p>
+     * Assert the sum result should be one.
+     */
+    @Test
+    public void ensureFivePlusFiveEquals10() {
+        //HACK: for demonstration purposes only
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+        // Arrange
+        int firsOperand = 5;
+        int secondOperand = 5;
+        int expectedResult = 10;
+        int result = 10;
+        // Act
+        result = new Calculator().sum(firsOperand, secondOperand);
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void ensureFivePlusNegativeFiveEqualsZero() {
+        //HACK: for demonstration purposes only
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+        // Arrange
+        int firsOperand = 5;
+        int secondOperand = -5;
+        int expectedResult = 0;
+        int result = 0;
+        // Act
+        result = new Calculator().sum(firsOperand, secondOperand);
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+    @Test
+    public void ensureFivePlusNegativeTenEqualsNegative5() {
+        //HACK: for demonstration purposes only
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+        // Arrange
+        int firsOperand = 5;
+        int secondOperand = -10;
+        int expectedResult = -5;
+        int result = -5;
+        // Act
+        result = new Calculator().sum(firsOperand, secondOperand);
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void ensureNegativeTwoPlusNegativeTwoEqualsNegativeFour() {
+        //HACK: for demonstration purposes only
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+        // Arrange
+        int firsOperand = -2;
+        int secondOperand = -2;
+        int expectedResult = -4;
+        int result = -4;
+        // Act
+        result = new Calculator().sum(firsOperand, secondOperand);
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void ensureOverflowIsCoughByException() {
+        //HACK: for demonstration purposes only
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+        // Arrange
+
+
+            int firsOperand = 999999999;
+            int secondOperand = 999999999;
+
+            // Act
+            int result = new Calculator().sum(firsOperand, secondOperand);
+        Assertions.assertThrows(ArithmeticException.class,
+                ()->{
+                    //do whatever you want to do here
+                    new Calculator().sum(firsOperand, secondOperand);
+                });
+
+
 }
 
+    @Test
+    public void ensureDivisionByZeroThrowsException() {
+        //HACK: for demonstration purposes only
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+        // Arrange
 
+
+        int firsOperand = 2;
+        int secondOperand = 0;
+        // Act
+        int result = new Calculator().sum(firsOperand, secondOperand);
+        Assertions.assertThrows(ArithmeticException.class,
+                ()->{
+                    //do whatever you want to do here
+                    new Calculator().divide(firsOperand, secondOperand);
+                });
+    }
+
+
+    @Test
+    public void ensureDivisionByZero() {
+        //HACK: for demonstration purposes only
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+        // Arrange
+
+
+        int firsOperand = 5;
+        int secondOperand = 1;
+        int expectedResult = 5;
+        // Act
+        int result = new Calculator().divide(firsOperand, secondOperand);
+
+        assertEquals(expectedResult, result);
+
+    }
+
+    @Test
+    public void ensureOverflowIsCoughByExceptionMin() {
+        //HACK: for demonstration purposes only
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+        // Arrange
+
+        int firsOperand = -999999999;
+        int secondOperand = -999999999;
+
+        // Act
+        Assertions.assertThrows(ArithmeticException.class,
+                ()->{
+                    //do whatever you want to do here
+                   new Calculator().sum(firsOperand, secondOperand);
+                });
+    }
+
+
+}
 
