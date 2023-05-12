8bb3b00f96d56426980cc3e5659f3899a18e35e3
thomas-lerchbaumer-fh <71784753+thomas-lerchbaumer-fh@users.noreply.github.com>
2023-05-12 18:10:56 +0200

    *basic Bookmark Design and setup

--- src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/BookmarkTest.java
+++ src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/BookmarkTest.java
@@ -1,4 +1,46 @@
 package pt.ipp.isep.dei.examples.tdd.basic.domain;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BookmarkTest {
+
+
+    @BeforeAll
+    public static void classSetUp() {
+        //HACK: for demonstration purposes only
+        System.out.println(
+                "This is a BookmarkTest class method and takes place before any @Test is executed");
+    }
+
+    @AfterAll
+    public static void classTearDown() {
+        //HACK: for demonstration purposes only
+        System.out.println(
+                "This is a BookmarkTest class method and takes place after all @Test are executed");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        //HACK: for demonstration purposes only
+        System.out.println(
+                "\tThis call takes place before each @Test is executed");
+    }
+
+    /**
+     * Test to ensure two positive numbers are summed correctly.<p>
+     * <p>
+     * For demonstration purposes the Arrange/Act/Assert syntax is used:<p>
+     * Arrange: one positive number (three) and another positive number (two).<p>
+     * Act: sum both numbers (three and two).<p>
+     * Assert: the result is five.
+     */
+
+
+
+
 }
