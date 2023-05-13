0163f22f91a190631edfb2d12767032d9d9dff91
thomas-lerchbaumer-fh <71784753+thomas-lerchbaumer-fh@users.noreply.github.com>
2023-05-13 16:48:12 +0200

    *US03 -  added test to check if other entries are not manipulated by RankingChange

--- src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/UserTest.java
+++ src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/UserTest.java
@@ -6,7 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class UserTest {
 
-
+    /**
+     * /US03. As a user when I add a duplicate<p>
+     * <p>
+     * bookmark, I want the system to increase the<p>
+     * /rating of that bookmark, because no exact.<p>
+     * Act: duplicates should exist.<p>
+     */
     @Test
     public void checkDuplicateBookmarkIsRankingIncrease() {
         System.out.println("\t\tExecuting " + new Object() {
@@ -55,6 +61,23 @@ public class UserTest {
         assertFalse(user.isDuplicate(bookmark1));
     }
 
+    @Test
+    public void checkIfRatingDidNotManipulatedOtherEntries(){
+        System.out.println("\t\tExecuting " + new Object() {
+        }.getClass().getEnclosingMethod().getName() + " Test");
+        //Arrange
 
+        User user = new User("John");
+        Bookmark bookmark = new Bookmark("https://www.google.com");
+        Bookmark bookmark1 = new Bookmark("https://www.yahoo.com");
+        Bookmark bookmark3 = new Bookmark("https://www.yahoo.com");
+        user.addBookmark(bookmark);
+        user.addBookmark(bookmark1);
+        //Act
+        user.addBookmark(bookmark3);
+        //Assert
+        assertEquals(0,user.getBookmarks().get(0).getRanking());
+
+    }
 
 }
